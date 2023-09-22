04d480ec7b7bbbe59991fee8883c07f6c33634bc
Frederik Henriksen <fbh@netcompany.com>
2023-09-22 10:20:42 +0200

    save

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,11 +6,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CreateEstablishmentModule } from './create-establishment/create-establishment.module';
+import { API_BASE_URL } from 'models';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LoginComponent, HomepageComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomepageComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, CreateEstablishmentModule],
+  providers: [{ provide: API_BASE_URL, useValue: 'https://localhost:44331' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
