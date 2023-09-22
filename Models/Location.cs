fb284cc30e58448d166bf175c4e7bced4b205035
Frederik Henriksen <fbh@netcompany.com>
2023-09-22 10:20:03 +0200

    Save

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YamlDotNet.Core.Tokens;
 
 namespace WebApplication1.Models
 {
     public class Location : EntityBase
     {
         public string Address { get; set; }
-
+        public Guid EstablishmentId { get; set; }
         public Establishment Establishment { get; set; }
     }
 
@@ -14,14 +15,12 @@ namespace WebApplication1.Models
         public void Configure(EntityTypeBuilder<Location> builder)
         {
             builder.ToTable(nameof(Location));  
-            builder.HasKey(l => l.Id);
 
             builder.Property(e => e.Address);
 
-            builder.HasOne(s => s.Establishment)
-            .WithOne(a => a.LocationId)
-            .HasPrincipalKey<Location>(a => a.Id)
-            .OnDelete(DeleteBehavior.Restrict);
+            //builder.HasOne(s => s.Establishment)
+            //.WithOne(a => a.Location)
+            //.HasForeignKey<Location>(a => a.Id);
         }
     }
 
